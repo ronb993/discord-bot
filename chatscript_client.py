6470d15838c7f81fe2e999742fa8c79cce13fc8f
verlooy <verlooy@gmail.com>
2022-05-01 15:10:28 -0500

    added test

--- chatscript_client.py
+++ chatscript_client.py
@@ -5,7 +5,7 @@ myHook = '' # place discord hook url here
 myAuthorization = '' # place authorization here
 channelID = '' # place discord channel ID here
 userName = '' # replace with discord username
-
+print("test")
 def sendAndReceiveChatScript(msgToSend, server='127.0.0.1', port=1024, timeout=10):
     try:
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
